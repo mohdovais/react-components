408240e11109f165f57d0ea82c94068ecc733386
Mohd Ovais <ovais.m@gmail.com>
2021-03-01 09:42:47 +0100

    space selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import 'focus-visible';
+import "focus-visible";
 import Calendar from "./components/calendar/Calendar";
 import {
   Segment,
@@ -12,12 +12,11 @@ function App(): JSX.Element {
   return (
     <div style={{ width: 1024, padding: "2em", margin: "0 auto" }}>
       <SegmentedControl value={color} onChange={setColor}>
-        {null}
         <Segment value="red">Red</Segment>
         <Segment value="blue">Blue</Segment>
         <Segment value="green">Green</Segment>
       </SegmentedControl>
-      <hr/>
+      <hr />
       <Calendar onSelect={(date) => console.log(date)} />
     </div>
   );
--- src/components/segmented-control/SegmentedControl.tsx
+++ src/components/segmented-control/SegmentedControl.tsx
@@ -17,26 +17,34 @@ interface SegmentedControlProps<T> {
 
 type ContextType<T> = {
   value: T;
+  focussed: T | null;
   onChange: (value: T) => void;
 };
 
 const SegmentContext = React.createContext<ContextType<any>>({
   value: undefined,
+  focussed: null,
   onChange: (value: any) => {},
 });
 
 export function Segment<T>(props: SegmentProps<T>): JSX.Element {
   const { value, children } = props;
-  const { value: selectedValue, onChange } = React.useContext<ContextType<T>>(
-    SegmentContext
-  );
+  const { value: selectedValue, focussed, onChange } = React.useContext<
+    ContextType<T>
+  >(SegmentContext);
   const selected = value === selectedValue;
   return (
     <div
       role="radio"
       aria-checked={selected}
       onClick={() => onChange(value)}
-      className={css.segment + " " + (selected ? css.selected : "")}
+      className={
+        css.segment +
+        " " +
+        (selected ? css.selected : "") +
+        " " +
+        (focussed === value ? css.focussed : "")
+      }
     >
       {children}
     </div>
@@ -47,28 +55,54 @@ function isSegment(node: React.ReactNode): node is JSX.Element {
   return React.isValidElement(node) && node.type === Segment;
 }
 
+const preventDefault = (event: React.KeyboardEvent) => event.preventDefault();
+
 export function SegmentedControl<T>(
   props: SegmentedControlProps<T>
 ): JSX.Element {
+  const [focussed, setFocussed] = React.useState<T | null>(null);
   const { onChange, value, children, id, className = "", style } = props;
   const values: T[] = React.Children.toArray(children)
     .filter(isSegment)
     .map((child) => child.props.value);
 
+  const onFocus = () => {
+    const isSeleced = values.indexOf(value) > -1;
+    setFocussed(isSeleced ? value : values[0]);
+  };
+
   const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     const length = values.length;
+    if (length === 0) {
+      return;
+    }
     switch (event.key) {
       case "ArrowDown":
       case "ArrowRight":
-        event.preventDefault();
-        onChange(values[(values.indexOf(value) + 1) % length]);
+        preventDefault(event);
+        setFocussed(
+          values[focussed == null ? 0 : (values.indexOf(focussed) + 1) % length]
+        );
         break;
       case "ArrowUp":
       case "ArrowLeft":
-        event.preventDefault();
-        onChange(values[(length + values.indexOf(value) - 1) % length]);
+        preventDefault(event);
+        setFocussed(
+          values[
+            focussed == null
+              ? 0
+              : (length + values.indexOf(focussed) - 1) % length
+          ]
+        );
         break;
+      case " ":
+        preventDefault(event);
+        focussed != null && focussed !== value && onChange(focussed);
     }
+  };
+
+  const onBlur = () => {
+    setFocussed(null);
   };
 
   return (
@@ -79,8 +113,10 @@ export function SegmentedControl<T>(
       style={style}
       onKeyDown={onKeyDown}
       tabIndex={0}
+      onFocus={onFocus}
+      onBlur={onBlur}
     >
-      <SegmentContext.Provider value={{ value, onChange }}>
+      <SegmentContext.Provider value={{ value, focussed, onChange }}>
         {children}
       </SegmentContext.Provider>
     </div>
